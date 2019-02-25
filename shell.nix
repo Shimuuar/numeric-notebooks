094d8d6a1ec0904fcad9df054c7a151d5d207024
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-02-25 21:32:16 +0300

    Another atttempt to fix notebooks

--- shell.nix
+++ shell.nix
@@ -1,6 +1,13 @@
 let
   pkgs   = import <nixpkgs> {inherit config;};
-  config = {};
+  config = {
+    packageOverrides = pkgs: {
+#      ihask = pkgs.callPackage ./nix/ihaskell-kernel.nix {};
+      haskell = pkgs.haskell // {
+        packageOverrides = haskOverrides;        
+      };
+    };
+  };
   # Python packages
   python = pkgs.python3;
   pyp = python.withPackages (ps: with ps;
@@ -10,34 +17,52 @@ let
     ]);
   # Haskell packages
   lib           = pkgs.haskell.lib;
-  haskOverrides = {
-    overrides = hsNew: hsOld: rec {
-      cereal          = lib.dontCheck hsOld.cereal;
-      aeson           = lib.dontCheck (hsNew.callPackage ./nix/aeson.nix {});
-      zeromq4-haskell = lib.dontCheck hsOld.zeromq4-haskell;
-      ipython-kernel  = hsNew.callPackage ./nix/ipython-kernel.nix {};
-      ihaskell        = lib.dontCheck (hsNew.callPackage ./nix/ihaskell.nix {});
-      ghc-parser      = hsNew.callPackage ./nix/ghc-parser.nix {};
-    };
+  haskOverrides = hsNew: hsOld: rec {
+    cereal          = lib.dontCheck hsOld.cereal;
+    aeson           = lib.dontCheck (hsNew.callPackage ./nix/aeson.nix {});
+    zeromq4-haskell = lib.dontCheck hsOld.zeromq4-haskell;
+    ipython-kernel  = hsNew.callPackage ./nix/ipython-kernel.nix {};
+    ihaskell        = lib.dontCheck (hsNew.callPackage ./nix/ihaskell.nix {});
+    ghc-parser      = hsNew.callPackage ./nix/ghc-parser.nix {};
   };
-  haskellPackages = pkgs.haskell.packages.ghc863.override haskOverrides;
-  haskp           = haskellPackages.ghcWithPackages (hp: with hp;
-    [ ipython-kernel
-      ihaskell
-      #
-      math-functions
+  #
+  haskp = pkgs.haskellPackages.ghcWithPackages (hp: with hp;
+    [ math-functions
       primitive
+      base58-bytestring
+#      ihaskell
+#      #
+#      math-functions
+#      primitive
     ]);
+
+#  ihaskellKernel = stdenv.mkDerivation {
+#    name = "ihaskell-kernel";
+#    phases = "installPhase";
+#    src = ./haskell.svg;
+#    buildInputs = [ ihaskellEnv ];
+#    installPhase = ''
+#      mkdir -p $out/kernels/ihaskell_${name}
+#      cp $src $out/kernels/ihaskell_${name}/logo-64x64.svg
+#      echo '${builtins.toJSON kernelFile}' > $out/kernels/ihaskell_${name}/kernel.json
+#    '';
+#  };
+  
 in
   pkgs.stdenv.mkDerivation {
     name        = "shell";
     buildInputs = [ pyp
                     haskp
+                    pkgs.haskellPackages.ihaskell
+#(pkgs.callPackage ./nix/ihaskell-kernel.nix
+#    {packages = hp: with hp; [primitive base58-bytestring];}
+#    )
                   ];
     #
     shellHook =
       ''
       export GHC_PACKAGE_PATH=$(echo ${haskp}/lib/*/package.conf.d| tr ' ' ':')
+      export PATH=$(echo ${haskp}/bin):$PATH
       export XDG_DATA_HOME=$PWD/.XDG
       export JUPYTER_CONFIG_DIR=$PWD/.XDG/jupyter
       '';
